fb8379335fec7b43ba263d17061c7b55d73680a9
Ibrahim <60303471@udst.edu.qa>
2026-02-12 14:58:35 +0300

    Fix lives counter calculation (fixes #7)

--- script.js
+++ script.js
@@ -239,7 +239,7 @@ function updateWrongLetters() {
 }
 
 function updateLives() {
-    const livesLeft = gameState.maxWrong - gameState.wrongGuesses + 1;
+    const livesLeft = gameState.maxWrong - gameState.wrongGuesses;
     document.getElementById('livesLeft').textContent = livesLeft;
 }
 
